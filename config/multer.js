9713808808a0ab404b9f8bea12ad50573b954c3d
Samuel <you@example.com>
2022-09-28 00:45:27 +0200

    modif multer pour envoie des photos dans le client

--- config/multer.js
+++ config/multer.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'upload/imagePlante')
+        cb(null, 'client/public/images/uploads/plantes')
     },
 
     filename: function (req, file, cb) {
